b25ecabeac6ea13b7f1e2d134ec483906f51ebf3
bokoness <bokoness@gmail.com>
2022-07-20 03:28:19 +0300

    v2 model creator added

--- server/scripts/createModel.js
+++ server/scripts/createModel.js
@@ -8,74 +8,6 @@ const { dirname } = path
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const createCrud = async () => {
-  try {
-    const basepath = path.join(__dirname, '..')
-    console.log(basepath)
-    const crudAns = await inquirer.prompt(modelQues)
-    const model = crudAns.modelName
-    createSchema(model, basepath)
-    createModel(model, basepath)
-    createPolicy(model, basepath)
-    createController(model, basepath)
-    createRouter(model, basepath)
-  } catch (e) {
-    console.log(`something bad happend, ${e}`)
-  }
-}
-
-const createSchema = (name, basepath) => {
-  const temp = `import mongoose from "mongoose";\nconst Schema = mongoose.Schema;\n\nconst ${name}Schema = new Schema({});\n\nexport default ${name}Schema
-  `
-  fs.writeFileSync(
-    `${basepath}/models/schemas/${name}Schema.js`,
-    temp,
-    'utf-8',
-  )
-  console.log(chalk.green(`${name} schema created`))
-}
-
-const createModel = (name, basepath) => {
-  const cname = capitalize(name)
-  const temp = `import mongoose from "mongoose"\nimport ${name}Schema from "./schemas/${name}Schema.js"\nconst ${cname} = mongoose.model("${name}", ${name}Schema)\n\nexport default ${cname}
-  `
-  fs.writeFileSync(`${basepath}/models/${name}Model.js`, temp, 'utf-8')
-  console.log(chalk.green(`${name} model created`))
-}
-
-const createPolicy = (name, basepath) => {
-  const cname = capitalize(name)
-  const temp = 'import Policy from "./Policy.js";\nexport default class extends Policy {}'
-  fs.writeFileSync(`${basepath}/policies/${cname}Policy.js`, temp, 'utf-8')
-  console.log(chalk.green(`${name} policy created`))
-}
-
-const createController = (name, basepath) => {
-  const cname = capitalize(name)
-  const temp = `import Controller from "./${cname}Controller.js";\nimport ${cname}Repository from "../repositories/${cname}Repository.js";\nimport ${cname}Policy from "../policies/${cname}Policy.js";\n\nconst repository = new ${cname}Repository();\n\nclass ${cname}Controller extends Controller {\n\tconstructor() {\n\t\tsuper({ name: "${name}", repository, policy: ${cname}Policy });\n\t}\n}\n\nexport default ${cname}Controller;
-  `
-
-  fs.writeFileSync(
-    `${basepath}/controllers/${cname}Controller.js`,
-    temp,
-    'utf-8',
-  )
-  console.log(chalk.green(`${name} controller created`))
-}
-
-const createRouter = (name, basepath) => {
-  const cname = capitalize(name)
-  try {
-    const temp = `import express from "express";\nimport Controller from "../controllers/${cname}Controller.js";\nimport { auth } from "../middleware/auth.js";\n\nconst router = express.Router();\n\nconst c = new Controller();\n//Basic Crud routes\n\nrouter.get("/", c.index());\nrouter.get("/:id", c.show());\nrouter.post("/", c.store());\nrouter.put("/:id", c.update());\nrouter.delete("/:id", c.destroy());\nrouter.post("/replicate/:id", c.replicate());
-\n\nexport default router;
-    `
-    fs.writeFileSync(`${basepath}/routes/${name}Routes.js`, temp, 'utf-8')
-    console.log(chalk.green(`${name} router created`))
-  } catch (e) {
-    throw e
-  }
-}
-
 const capitalize = (str) => {
   str = str.toLowerCase()
   return str.charAt(0).toUpperCase() + str.slice(1)
@@ -102,5 +34,83 @@ const modelQues = [
     validate: validators.notEmpty,
   },
 ]
+
+const createSchema = (name, basepath) => {
+  const cname = capitalize(name)
+  let template = fs
+    .readFileSync(path.join(__dirname, 'templates', 'TemplateSchema.js'))
+    .toString()
+  template = template.replaceAll('template', name)
+  template = template.replaceAll('Template', cname)
+  fs.writeFileSync(`${basepath}/models/schemas/${name}Schema.js`, template, 'utf-8')
+  console.log(chalk.green(`${name} schema created`))
+}
+
+const createModel = (name, basepath) => {
+  const cname = capitalize(name)
+  let template = fs
+    .readFileSync(path.join(__dirname, 'templates', 'TemplateModel.js'))
+    .toString()
+  template = template.replaceAll('template', name)
+  template = template.replaceAll('Template', cname)
+  fs.writeFileSync(`${basepath}/models/${cname}Model.js`, template, 'utf-8')
+  console.log(chalk.green(`${name} model created`))
+}
+
+const createPolicy = (name, basepath) => {
+  const cname = capitalize(name)
+  let template = fs
+    .readFileSync(path.join(__dirname, 'templates', 'TemplatePolicy.js'))
+    .toString()
+  template = template.replaceAll('template', name)
+  template = template.replaceAll('Template', cname)
+  fs.writeFileSync(`${basepath}/policies/${cname}Policy.js`, template, 'utf-8')
+  console.log(chalk.green(`${name} policy created`))
+}
+
+const createController = (name, basepath) => {
+  const cname = capitalize(name)
+  let template = fs
+    .readFileSync(path.join(__dirname, 'templates', 'TemplateController.js'))
+    .toString()
+  template = template.replaceAll('template', name)
+  template = template.replaceAll('Template', cname)
+  fs.writeFileSync(
+    `${basepath}/controllers/${cname}Controller.js`,
+    template,
+    'utf-8',
+  )
+  console.log(chalk.green(`${name} controller created`))
+}
+
+const createRouter = (name, basepath) => {
+  const cname = capitalize(name)
+  let template = fs
+    .readFileSync(path.join(__dirname, 'templates', 'TemplateRouter.js'))
+    .toString()
+  template = template.replaceAll('template', name)
+  template = template.replaceAll('Template', cname)
+  fs.writeFileSync(`${basepath}/routes/${name}Routes.js`, template, 'utf-8')
+  console.log(chalk.green(`${name} router created`))
+}
+
+const createCrud = async () => {
+  try {
+    const basepath = path.join(__dirname, '..')
+    const crudAns = await inquirer.prompt(modelQues)
+    const model = crudAns.modelName.toLowerCase()
+    const cname = capitalize(model)
+    createSchema(model, basepath)
+    createModel(model, basepath)
+    createPolicy(model, basepath)
+    createController(model, basepath)
+    createRouter(model, basepath)
+    console.log(chalk.blue(`${cname} model created completely`))
+    console.log(chalk.blue(`Please import ${model}Routes.js into /routes/routes.js file`))
+    console.log(chalk.blue(`Please modify ${cname}Schema.js and add the wanted field`))
+  } catch (e) {
+    console.log(`something bad happend, ${e}`)
+  }
+}
 
 createCrud()
